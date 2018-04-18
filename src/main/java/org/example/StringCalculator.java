f617107fdcd29fc6d9539312a5c2bc61206f7984
pietre <pietre.pietre.pietre@gmail.com>
2018-04-18 20:50:27 +0200

    StringCalculator class refactor

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -8,20 +8,45 @@ public class StringCalculator {
     private static final String DEFAULT_SPLIT_PATTERN = ",|\n";
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
 
+    private static final int CUSTOM_DELIMITER_CAPTURED_GROUP_INDEX = 1;
+    private static final int NUMBERS_CAPTURED_GROUP_INDEX = 2;
+
     public static int add(final String numbers) {
-        String numbersString = numbers;
-        String splitPattern = DEFAULT_SPLIT_PATTERN;
-        if (numbersString.isEmpty()) {
+        if (numbers.isEmpty()) {
             return 0;
         }
 
-        Pattern r = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
-        Matcher m = r.matcher(numbersString);
-        if (m.find()) {
-            splitPattern += "|" + m.group(1);
-            numbersString = m.group(2);
+        return calculateSum(numbers);
+    }
+
+    private static int calculateSum(final String numbers) {
+        String numbersString = numbers;
+        String splitPattern = DEFAULT_SPLIT_PATTERN;
+
+        Matcher matcher = getCustomDelimiterMatcher(numbersString);
+        if (matcher.find()) {
+            splitPattern += appendCustomDelimiter(matcher);
+            numbersString = getNumbers(matcher);
         }
-        
+
+        return addNumbersSplitByPattern(numbersString, splitPattern);
+    }
+
+    private static Matcher getCustomDelimiterMatcher(String numbersString) {
+        Pattern r = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
+
+        return r.matcher(numbersString);
+    }
+
+    private static String getNumbers(Matcher matcher) {
+        return matcher.group(NUMBERS_CAPTURED_GROUP_INDEX);
+    }
+
+    private static String appendCustomDelimiter(Matcher matcher) {
+        return "|" + matcher.group(CUSTOM_DELIMITER_CAPTURED_GROUP_INDEX);
+    }
+
+    private static int addNumbersSplitByPattern(String numbersString, String splitPattern) {
         int sum = 0;
         for (String number : numbersString.split(splitPattern)) {
             sum += Integer.parseInt(number);
