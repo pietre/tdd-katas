b56bf2464926164ae83eeafc622b0e3982814567
pietre <pietre.pietre.pietre@gmail.com>
2018-04-19 21:02:29 +0200

    small refactor

--- src/main/java/org/example/stringcalculator/StringCalculator.java
+++ src/main/java/org/example/stringcalculator/StringCalculator.java
@@ -7,11 +7,13 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    private static final String DEFAULT_SPLIT_PATTERN = ",|\n";
+    private static final String CUSTOM_DELIMITER_DELIMITER = "\n";
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.+)\n(.*)";
+    private static final String DEFAULT_SPLIT_PATTERN = ",|\n";
+    private static final String NEGATIVE_INTEGER_PATTERN = "-\\d+";
 
     private static final int CUSTOM_DELIMITER_CAPTURED_GROUP_INDEX = 1;
-    private static final int NUMBERS_CAPTURED_GROUP_INDEX = 2;
+
 
     public static int add(final String numbers) {
         if (numbers.isEmpty()) {
@@ -41,7 +43,7 @@ public class StringCalculator {
     }
 
     private static String getNumbersWithoutCustomDelimiter(String numbersString) {
-        return numbersString.substring((numbersString.indexOf("\n") + 1));
+        return numbersString.substring((numbersString.indexOf(CUSTOM_DELIMITER_DELIMITER) + 1));
     }
 
     private static String appendCustomDelimiter(Matcher matcher) {
@@ -52,7 +54,7 @@ public class StringCalculator {
         int sum = 0;
         List<String> negativeNumbers = new ArrayList<>();
         for (String number : numbersString.split(splitPattern)) {
-            if (number.matches("-\\d+")) {
+            if (number.matches(NEGATIVE_INTEGER_PATTERN)) {
                 negativeNumbers.add(number);
             }
             sum += Integer.parseInt(number);
