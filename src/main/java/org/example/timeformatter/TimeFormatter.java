6d7cfe70c5bac7255bd642181d895b5f4402dca0
pietre <pietre.pietre.pietre@gmail.com>
2018-06-21 09:42:53 +0200

    TimeFormatter: created reusable methods and added tests for formatting minutes

--- src/main/java/org/example/timeformatter/TimeFormatter.java
+++ src/main/java/org/example/timeformatter/TimeFormatter.java
@@ -1,17 +1,60 @@
 package org.example.timeformatter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeFormatter {
 
+    private static final int SECONDS_PER_MINUTE = 60;
+    private List<String> formattedUnitsList = new ArrayList<>();
+
+    private TimeFormatter() {
+    }
+
     public static String formatDuration(int seconds) {
-        String formatted = "";
+        TimeFormatter formatter = new TimeFormatter();
 
-        if (seconds == 1) {
-            formatted += seconds + " second";
+        return formatter.getFormattedDuration(seconds);
+    }
+
+    private String getFormattedDuration(int seconds) {
+        if (seconds >= SECONDS_PER_MINUTE) {
+            int minutes = seconds / SECONDS_PER_MINUTE;
+            seconds %= SECONDS_PER_MINUTE;
+            appendFormattedUnit(getFormattedUnit(minutes, "minute"));
         }
-        else {
-            formatted += seconds + " seconds";
+        appendFormattedUnit(getFormattedUnit(seconds, "second"));
+
+        return joinFormattedUnitsList();
+    }
+
+    private String getFormattedUnit(int value, String singleUnitName) {
+        if (value == 0) {
+            return "";
+        } else if (value == 1) {
+            return value + " " + singleUnitName;
+        }
+        return value + " " + singleUnitName + "s";
+    }
+
+    private void appendFormattedUnit(String formattedUnit) {
+        if (!formattedUnit.isEmpty()) {
+            formattedUnitsList.add(formattedUnit);
+        }
+    }
+
+    private String joinFormattedUnitsList() {
+        StringBuilder resultBuilder = new StringBuilder();
+        int listSize = formattedUnitsList.size();
+        for (int i = 0; i < listSize; i++) {
+            resultBuilder.append(formattedUnitsList.get(i));
+            if (i < listSize - 2) {
+                resultBuilder.append(" , ");
+            } else if (i == listSize - 2) {
+                resultBuilder.append(" and ");
+            }
         }
 
-        return formatted;
+        return resultBuilder.toString();
     }
 }
--- src/test/java/org/example/TimeFormatterTest.java
+++ src/test/java/org/example/TimeFormatterTest.java
@@ -3,7 +3,7 @@ package org.example;
 import org.example.timeformatter.TimeFormatter;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TimeFormatterTest {
 
@@ -25,5 +25,21 @@ public class TimeFormatterTest {
         assertEquals("20 seconds", result);
     }
 
+    @Test
+    public void returnsMinuteWithSeconds() {
+        int seconds = 70;
 
+        String result = TimeFormatter.formatDuration(seconds);
+
+        assertEquals("1 minute and 10 seconds", result);
+    }
+
+    @Test
+    public void returnsManyMinutes() {
+        int seconds = 120;
+
+        String result = TimeFormatter.formatDuration(seconds);
+
+        assertEquals("2 minutes", result);
+    }
 }
